c692f9a5d2c0cd35cf8a4a710f767bd2fc99859a
80386DX <etwas>
2024-01-06 16:28:21 +0100

    Step 1: Initialize project via Spring initializr

---
+++ src/main/java/com/heliant/spring/SpringZadatakApplication.java
@@ -0,0 +1,13 @@
+package com.heliant.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZadatakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliant/spring/SpringZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliant.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
